ff588e48e9aa0e44fa11dd51bf889987fb32074c
Aya Hamoudeh <ayahamoudeh29@gmail.com>
2025-07-03 01:11:30 +0300

    Remove sensitive file from repo

--- lib/core/sensitive_data.dart
+++
@@ -1,4 +0,0 @@
-const String supabaseUrl = "https://roukbxhlisygcczaneiw.supabase.co/rest/v1/";
-const String authUrl = 'https://roukbxhlisygcczaneiw.supabase.co/auth/v1/';
-const String anonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvdWtieGhsaXN5Z2NjemFuZWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMzEzOTcsImV4cCI6MjA1OTgwNzM5N30.9sN8CPKcwBcJW5KaJSDfBS8FkImIwD1jTntexbHJrqo';
